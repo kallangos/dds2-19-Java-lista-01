ea5f665828235844ff05a5b6f6ca50a1a5d97db3
kallangos <vandersondasilva@gmail.com>
2024-08-15 16:31:28 -0300

    Sistema de Aprovação de Alunos

--- EX01/src/main/java/com/mycompany/ex01/EX01.java
+++ EX01/src/main/java/com/mycompany/ex01/EX01.java
@@ -7,7 +7,10 @@ import java.util.Scanner;
 
 /**
  *
- * @author Thor
+ * 1. Sistema de Aprovação de Alunos: Crie um programa que receba as notas de um aluno em
+ * três disciplinas e calcule sua média. Utilize estruturas IF e ELSE para determinar se o aluno
+ * foi aprovado, reprovado ou está em recuperação com base na média (Aprovado: média ≥ 7,
+ * Recuperação: 5 ≤ média < 7, Reprovado: média < 5)
  */
 public class EX01 {
 
