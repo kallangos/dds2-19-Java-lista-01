17456ec9049af6db1b07e18ecaa979f3307f3c7c
kallangos <vandersondasilva@gmail.com>
2024-08-16 14:15:51 -0300

    Verificação de Palíndromo

---
+++ EX16/src/main/java/com/mycompany/ex16/EX16.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ex16;
+import java.util.Scanner;
+
+/**
+ *
+ *  Verificação de Palíndromo: Desenvolva um programa que solicite ao usuário uma 
+ * palavra e utilize um laço FOR para verificar se ela é um palíndromo. Utilize IF para 
+ * comparar os caracteres da palavra e determinar o resultado.
+ */
+public class EX16 {
+
+    public static void main(String[] args) {
+        String palindromo = "";
+        boolean ePalindromo = true;
+        int tamanhoPalavra = 0 ;
+        
+        Scanner ler = new Scanner(System.in);
+        
+        System.out.println("-----------------------------------------------------");
+        System.out.println("|           Verificação de Palíndromo               |");
+        System.out.println("-----------------------------------------------------");
+        System.out.println("|                                                   |");
+        System.out.println("|  Digite uma palavra : ");
+        palindromo = ler.nextLine();
+        tamanhoPalavra = palindromo.length();
+        
+        for (int i = 0; i < tamanhoPalavra / 2; i++) {
+            if (palindromo.charAt(i) != palindromo.charAt(tamanhoPalavra - 1 - i)) {
+                ePalindromo = false;
+                break;
+            }
+        }
+        if (ePalindromo==true){
+            System.out.println("|  É um Palíndromo. ");
+        } else {
+            System.out.println("|  Não é Palíndromo. ");
+        }
+        System.out.println("| ");
+        System.out.println("|  Saindo...");
+        
+    }
+}
