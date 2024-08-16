3262687fcedf974fd87d7a735f29fdac17fc3547
kallangos <vandersondasilva@gmail.com>
2024-08-16 09:41:25 -0300

    Sistema de Senha

--- EX08/src/main/java/com/mycompany/ex08/EX08.java
+++ EX08/src/main/java/com/mycompany/ex08/EX08.java
@@ -3,14 +3,37 @@
  */
 
 package com.mycompany.ex08;
+import java.util.Scanner;
 
 /**
  *
- * @author Thor
+ * Sistema de Senha: Desenvolva um programa que solicite ao usuário uma senha e utilize 
+ * IF-ELSE para verificar se ela atende aos critérios de segurança (mínimo de 8 caracteres). O 
+ * programa deve permitir ao usuário tentar novamente até que uma senha válida seja 
+ * inserida.
  */
 public class EX08 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String pwd = "pwd";
+        Scanner ler = new Scanner(System.in);
+        
+        while (pwd.length()<=8){
+            System.out.println("------------------------");
+            System.out.println("|  Senha Validator     |");
+            System.out.println("------------------------");
+            System.out.println("|  ");
+            System.out.println("| Entre com uma senha de no MINIMO 08 caracteres : ");
+            pwd = ler.nextLine();
+            if (pwd.length()<8){
+                System.out.println("|  ");
+                System.out.println("| Senha INVALIDA com apenas "+ pwd.length()+ " caracteres.");
+            } else {
+                System.out.println("|  ");
+                System.out.println("| Senha valida com "+ pwd.length()+ " caracteres.");
+                System.out.println("| Saindo...");
+                System.out.println("|  ");
+            }
+        }
     }
 }
