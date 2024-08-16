22fce1922e08c88cdfec1f71889792ae915ce113
kallangos <vandersondasilva@gmail.com>
2024-08-16 17:23:52 -0300

    Sistema de Pedágio

---
+++ EX18/src/main/java/com/mycompany/ex18/EX18.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ex18;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Thor
+ */
+public class EX18 {
+
+    public static void main(String[] args) {
+        // [0]carro,[1]moto,[2]caminhao       
+        double[] listaPedadios = {12.50,9.50,16.50}; 
+        int tipoVeiculo = 0;
+        Scanner ler = new Scanner(System.in);
+        
+        System.out.println("-----------------------------------------------------");
+        System.out.println("|              Systema de pedagio                   |");
+        System.out.println("-----------------------------------------------------");
+        System.out.println("|                                                   |");
+        System.out.println("|          Informe qual é o seu veículo            |");
+        System.out.println("|                                                   |");
+        System.out.println("|      (1) Carro   (2) Moto   (3) Caminhão          |");
+        System.out.println("| ");
+        tipoVeiculo = ler.nextInt();
+        if (tipoVeiculo==1){
+            System.out.println("| O Valor do pedagio é : R$ " + listaPedadios[0]);
+        } else if (tipoVeiculo==2){
+            System.out.println("| O Valor do pedagio é : R$ " + listaPedadios[1]);
+        } if (tipoVeiculo==3){
+            System.out.println("| O Valor do pedagio é : R$ " + listaPedadios[2]);
+        }
+        
+        System.out.println("| ");
+        System.out.println("| Saindo...");
+        
+    }
+}
